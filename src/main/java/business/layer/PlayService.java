37fbc77ede31736c6369fe3eb1e7ce8edca3fabe
Hrafnkell <hrafnkellnk15@ru.is>
2016-10-26 19:38:42 +0000

    Testing the travis gradle check task

--- src/main/java/business/layer/PlayService.java
+++ src/main/java/business/layer/PlayService.java
@@ -4,4 +4,10 @@ package business.layer;
  * Created by Hrafnkell on 26/10/2016.
  */
 public class PlayService {
+
+    public static String TravisTester()
+    {
+        return "Hello World";
+    }
+
 }
--- src/test/java/business/layer/PlayServiceTest.java
+++ src/test/java/business/layer/PlayServiceTest.java
@@ -6,4 +6,11 @@ import static org.junit.Assert.assertEquals;
  * Created by Hrafnkell on 26/10/2016.
  */
 public class PlayServiceTest {
+
+    @Test
+    public void testHelloWorld()
+    {
+        assertEquals("Hello World", PlayService.TravisTester());
+    }
+
 }
