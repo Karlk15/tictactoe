f94d364be3ab9684dee8b544aefb6a88d29899be
Óli Ólafsson <oliolafsson@Olis-MacBook-Pro.local>
2016-10-27 22:28:49 +0000

    New function Player added checking if player is computer or human.

--- src/main/java/business/layer/Player.java
+++ src/main/java/business/layer/Player.java
@@ -1,9 +1,27 @@
 package business.layer;
-
+import java.util.Scanner;
 /**
  * Created by oliolafsson on 27/10/16.
  */
 public class Player
 {
+    private String playerName;
+    private int playerNumber;
+    private boolean humanPlayer;
+
+
+    public Player(String name, int number, boolean isHuman)
+    {
+        if(!isHuman)
+        {
+            playerName = "DickButt";
+        }
+        else
+        {
+            this.playerName = name;
+        }
+        this.playerNumber = number;
+        humanPlayer = isHuman;
+    }
 
 }
