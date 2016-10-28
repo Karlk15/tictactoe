032a6e1669cded8ae056fee94df2aa3965b24cdc
valdimar94 <valdimarjonsson94@gmail.com>
2016-10-28 00:51:36 +0000

    Added an algorithm to check win, will need further implementation as it returns nothing

--- src/main/java/business/layer/PlayService.java
+++ src/main/java/business/layer/PlayService.java
@@ -6,8 +6,7 @@ import java.util.Scanner;
 /**
  * Created by Hrafnkell on 26/10/2016.
  */
-public class PlayService
-{
+public class PlayService {
     public enum playerChar {
         X, O
     }
@@ -15,41 +14,66 @@ public class PlayService
     private String[] board;
     private playerChar currentPlayer;
 
-    public PlayService()
-    {
+    public PlayService() {
         board = new String[9];
         currentPlayer = currentPlayer.X;
-        for(int i = 0; i < 9; i++)
-            board[i] = Integer.toString(i+1);
+        for (int i = 0; i < 9; i++)
+            board[i] = Integer.toString(i + 1);
     }
 
-    public int getSize()
-    {
+    public int getSize() {
         return 9;
     }
 
-    public String getNext(int next)
-    {
+    public String getNext(int next) {
         return board[next];
     }
 
-    public int PlayTurn(char input)
-    {
+    public int PlayTurn(char input) {
         return 0;
     }
-<<<<<<< HEAD
 
-    private void results(){
+    private void results() {
 
-        
+        int rowCount = 0;
+
+        for (int i = 0; i < board.length; i++){
+            if (rowCount % 3 == 0){
+                // win
+            }
+            if (i + 1 % 3 == 0) {
+                rowCount = 0;
+            }
+            if (board[i].equals(currentPlayer)){
+                rowCount++;
+            }
+        }
+        for (int j = 0; j < board.length; j+= 3){
+            if (j >= 6 && j <= 8){
+                j -= 5;
+            }
+            if (rowCount % 3 == 0){
+                // win
+            }
+            if (j % 3 == 0) {
+                rowCount = 0;
+            }
+            if (board[j].equals(currentPlayer)){
+                rowCount++;
+            }
+        }
+        if (board[0].equals(currentPlayer) && board[4].equals(currentPlayer) && board[8].equals(currentPlayer)){
+            // win
+        }
+        if (board[2].equals(currentPlayer) && board[4].equals(currentPlayer) && board[6].equals(currentPlayer)){
+            // win
+        }
     }
 
-    private void togglePlayer(){
+    private void togglePlayer() {
 
-
+        if (currentPlayer == playerChar.X){
+            currentPlayer = playerChar.O;
+        }
     }
-
-
-=======
->>>>>>> efc71e12161cbb6f1ab19448ee7b5775e631dd2a
 }
